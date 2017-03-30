0a13a52de45e9edf96dbe567e69fc29247b5b479
Jan Schwien <jan5366@gmail.com>
2017-03-30 13:10:26 +0200

    started work

---
+++ SimpleCharcoal/prototypes/furnance-recipe.lua
@@ -0,0 +1,18 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: jschwien
+-- Date: 30.03.2017
+--
+data:extend(
+    {
+        {
+            type = "recipe",
+            name = "charcoal_burning",
+            category = "smelting",
+            enabled = true,
+            energy_required = 120.0,
+            ingredients = {{"wood", 1}},
+            result = "coal"
+        }
+    }
+)
